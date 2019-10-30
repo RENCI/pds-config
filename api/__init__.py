1ec2c925c25fdd2805346386e5cad3b6b4fdf7b7
Hao Xu <xuh@cs.unc.edu>
2019-10-29 22:39:45 -0400

    update

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 from pymongo import MongoClient
 
 mongodb_host = os.environ["MONGO_HOST"]
@@ -7,6 +8,9 @@ mongo_database = os.environ["MONGO_DATABASE"]
 mongo_collection = os.environ["MONGO_COLLECTION"]
 mongo_username = os.environ["MONGO_NON_ROOT_USERNAME"]
 mongo_password = os.environ["MONGO_NON_ROOT_PASSWORD"]
+default_config = os.environ.get("DEFAULT_CONFIG")
+if default_config is not None and default_config != "":
+    default_config = json.loads(default_config)
 
 mongo_client = MongoClient(mongodb_host, mongodb_port, username=mongo_username, password=mongo_password, authSource=mongo_database)
 
@@ -14,6 +18,8 @@ def getConfig():
     config = mongo_client[mongo_database][mongo_collection].find_one()
     if config is not None:
         del config["_id"]
+    else:
+        config = default_config
     return config
 
 def putConfig(body):
